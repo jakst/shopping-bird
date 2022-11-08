2a0d38562e2dbdd91738eba5580f07ee330a8965
Jakob Norlin <jakob.stahl91@gmail.com>
2022-11-08 14:07:44 +0100

    Revert "BUMP"
    This reverts commit 267cdc44fa2a4e5f5805a4ab4522ee73da486f19.

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -63,7 +63,7 @@ export default function Home() {
       </Show>
 
       <h1 class="text-center text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello Bird!1
+        Hello Bird!
       </h1>
 
       <div class="flex justify-end">
