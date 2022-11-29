1666632c9c0e9a1a3351e85f4d1154a1f3f5eae5
Jakob Norlin <jakob.stahl91@gmail.com>
2022-11-29 19:41:39 +0100

    Run sync on every new client connection

--- server/src/sseClients.ts
+++ server/src/sseClients.ts
@@ -26,8 +26,9 @@ export function createClient(id: Client["id"], reply: Client["reply"]) {
   clients.push({ id, reply });
   console.log(`${id} connection opened (${clients.length} total)`);
 
-  if (clients.length === 1) {
-    setWorkerInterval(60_000); // Every minute
+  if (clients.length >= 1) {
+    runSyncWorker(); // Run once when client connects...
+    setWorkerInterval(60_000); // ... then every minute
   }
 }
 
