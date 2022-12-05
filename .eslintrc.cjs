59d5bcad8ef32a16cc104a89e5842f36232d7402
Jakob Norlin <jakob.stahl91@gmail.com>
2022-12-05 20:41:20 +0100

    Remove `no-unused-vars` and `no-empty-function` eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,9 @@
 const disableOverZealousRules = {
   "no-empty": "off",
+  "no-unused-vars": "off",
+
+  "@typescript-eslint/no-unused-vars": "off",
+  "@typescript-eslint/no-empty-function": "off",
   "@typescript-eslint/no-floating-promises": "off",
   "@typescript-eslint/no-explicit-any": "off",
   "@typescript-eslint/no-unsafe-argument": "off",
