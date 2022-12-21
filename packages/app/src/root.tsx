11318f3a5e49a2d6339f088a1fc7bbc7d2fb7f0e
Jakob Norlin <jakob.stahl91@gmail.com>
2022-12-21 16:48:40 +0100

    Write a proper HTML title

--- packages/app/src/root.tsx
+++ packages/app/src/root.tsx
@@ -18,7 +18,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        <Title>Hello Bird - Your shopping list</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
 
