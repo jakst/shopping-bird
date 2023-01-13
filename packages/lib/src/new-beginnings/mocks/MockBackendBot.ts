b98e581605c2819d7925cce8c94d1bc1e15f2174
Jakob Norlin <jakob.stahl91@gmail.com>
2023-01-13 12:34:28 +0100

    Decrease backend bot pause to 1ms

--- packages/lib/src/new-beginnings/mocks/MockBackendBot.ts
+++ packages/lib/src/new-beginnings/mocks/MockBackendBot.ts
@@ -11,7 +11,7 @@ export class MockBackendBot implements BackendClientBot {
   async ADD_ITEM(name: string) {
     console.log("[BACKEND_CLIENT] ADD_ITEM", name);
 
-    await new Promise((resolve) => setTimeout(resolve, 10));
+    await new Promise((resolve) => setTimeout(resolve, 1));
     if (!this.shoppingList.some((item) => item.name === name))
       this.shoppingList.push({ name, checked: false });
   }
@@ -19,7 +19,7 @@ export class MockBackendBot implements BackendClientBot {
   async DELETE_ITEM(name: string) {
     console.log("[BACKEND_CLIENT] DELETE_ITEM", name);
 
-    await new Promise((resolve) => setTimeout(resolve, 10));
+    await new Promise((resolve) => setTimeout(resolve, 1));
     const index = this.shoppingList.findIndex((item) => item.name === name);
     if (index >= 0) this.shoppingList.splice(index, 1);
   }
@@ -27,7 +27,7 @@ export class MockBackendBot implements BackendClientBot {
   async RENAME_ITEM(oldName: string, newName: string) {
     console.log("[BACKEND_CLIENT] RENAME_ITEM", { oldName, newName });
 
-    await new Promise((resolve) => setTimeout(resolve, 10));
+    await new Promise((resolve) => setTimeout(resolve, 1));
     const item = this.shoppingList.find((item) => item.name === oldName);
     if (item) item.name = newName;
   }
@@ -35,7 +35,7 @@ export class MockBackendBot implements BackendClientBot {
   async SET_ITEM_CHECKED(name: string, checked: boolean) {
     console.log("[BACKEND_CLIENT] SET_ITEM_CHECKED", { name, checked });
 
-    await new Promise((resolve) => setTimeout(resolve, 10));
+    await new Promise((resolve) => setTimeout(resolve, 1));
     const item = this.shoppingList.find((item) => item.name === name);
     if (item) item.checked = checked;
   }
@@ -43,7 +43,7 @@ export class MockBackendBot implements BackendClientBot {
   async CLEAR_CHECKED_ITEMS() {
     console.log("[BACKEND_CLIENT] CLEAR_CHECKED_ITEMS");
 
-    await new Promise((resolve) => setTimeout(resolve, 10));
+    await new Promise((resolve) => setTimeout(resolve, 1));
     for (let i = this.shoppingList.length - 1; i >= 0; i--) {
       if (this.shoppingList[i].checked) this.shoppingList.splice(i, 1);
     }
