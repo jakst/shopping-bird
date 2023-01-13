779cec878cf26510eb22be4a894c07b96dae5167
Jakob Norlin <jakob.stahl91@gmail.com>
2023-01-13 14:59:01 +0100

    Remove console.logs in mocked backend bot

--- packages/lib/src/new-beginnings/test-utils/MockBackendBot.ts
+++ packages/lib/src/new-beginnings/test-utils/MockBackendBot.ts
@@ -10,40 +10,30 @@ export class MockBackendBot implements BackendClientBot {
   }
 
   async ADD_ITEM(name: string) {
-    console.log("[BACKEND_CLIENT] ADD_ITEM", name);
-
     await pause(1);
     if (!this.shoppingList.some((item) => item.name === name))
       this.shoppingList.push({ name, checked: false });
   }
 
   async DELETE_ITEM(name: string) {
-    console.log("[BACKEND_CLIENT] DELETE_ITEM", name);
-
     await pause(1);
     const index = this.shoppingList.findIndex((item) => item.name === name);
     if (index >= 0) this.shoppingList.splice(index, 1);
   }
 
   async RENAME_ITEM(oldName: string, newName: string) {
-    console.log("[BACKEND_CLIENT] RENAME_ITEM", { oldName, newName });
-
     await pause(1);
     const item = this.shoppingList.find((item) => item.name === oldName);
     if (item) item.name = newName;
   }
 
   async SET_ITEM_CHECKED(name: string, checked: boolean) {
-    console.log("[BACKEND_CLIENT] SET_ITEM_CHECKED", { name, checked });
-
     await pause(1);
     const item = this.shoppingList.find((item) => item.name === name);
     if (item) item.checked = checked;
   }
 
   async CLEAR_CHECKED_ITEMS() {
-    console.log("[BACKEND_CLIENT] CLEAR_CHECKED_ITEMS");
-
     await pause(1);
     for (let i = this.shoppingList.length - 1; i >= 0; i--) {
       if (this.shoppingList[i].checked) this.shoppingList.splice(i, 1);
