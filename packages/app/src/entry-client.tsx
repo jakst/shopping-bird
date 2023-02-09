76648f4924388ecf61b9e8d8e0b361abe937bc4e
Jakob Norlin <jakob.stahl91@gmail.com>
2023-02-09 08:30:39 +0100

    Add button

--- packages/app/src/entry-client.tsx
+++ packages/app/src/entry-client.tsx
@@ -1,50 +1,6 @@
 import { mount, StartClient } from "solid-start/entry-client"
 
-let deferredPrompt: Event | null = null
-
-//reference to your install button
-
-//We hide the button initially because the PWA will not be available for
-//install until it follows the A2HS criteria.
-// addBtn.style.display = 'none';
-
-let onClick: (event: Event) => void
-
-window.addEventListener("beforeinstallprompt", (e) => {
-	console.log("[swrkr] beforeInstall")
-	// Prevent Chrome 67 and earlier from automatically showing the prompt
-	e.preventDefault()
-	// Stash the event so it can be triggered later.
-	deferredPrompt = e
-	// Update UI to notify the user they can add to home screen
-	// addBtn.style.display = 'block';
-
-	onClick = (e) => {
-		// hide our user interface that shows our A2HS button
-		// addBtn.style.display = "none"
-		// Show the prompt
-		deferredPrompt.prompt()
-		// Wait for the user to respond to the prompt
-		deferredPrompt.userChoice.then((choiceResult) => {
-			if (choiceResult.outcome === "accepted") {
-				console.log("User accepted the A2HS prompt")
-			} else {
-				console.log("User dismissed the A2HS prompt")
-			}
-			deferredPrompt = null
-		})
-	}
-})
-
-mount(
-	() => (
-		<>
-			<StartClient />
-			<button onClick={onClick}>Derp</button>
-		</>
-	),
-	document,
-)
+mount(() => <StartClient />, document)
 
 if ("serviceWorker" in navigator) {
 	console.log("[swrkr] supported")
