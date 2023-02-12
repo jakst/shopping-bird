59ed50ef8802ee40c0dff49a6cbcc9c9b6727c8c
Jakob Norlin <jakob.stahl91@gmail.com>
2023-02-12 18:26:51 +0100

    Manifest

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -7,7 +7,21 @@ import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
 	plugins: [
-		VitePWA({ registerType: "autoUpdate" }),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				short_name: "Shopping Bird",
+				name: "Shopping Bird",
+				icons: [
+					{ src: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+					{ src: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+				],
+				start_url: "/",
+				theme_color: "#0369a1",
+				background_color: "#fff",
+				display: "minimal-ui",
+			},
+		}),
 		solid({ adapter: vercel({ edge: true }) }),
 		Icons({ compiler: "solid", autoInstall: true }),
 	],
