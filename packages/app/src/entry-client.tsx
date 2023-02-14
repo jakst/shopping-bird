262a83f96b87d12dc05535a21be085c1a01ccd54
Jakob Norlin <jakob.stahl91@gmail.com>
2023-02-14 23:24:52 +0100

    Remove useLocator call from entry-client
    It was spamming the console with `solid-devtools useLocator can be called only once.`

--- packages/app/src/entry-client.tsx
+++ packages/app/src/entry-client.tsx
@@ -1,9 +1,6 @@
-import { useLocator } from "solid-devtools"
 import { mount, StartClient } from "solid-start/entry-client"
 import { registerSW } from "virtual:pwa-register"
 
 if ("serviceWorker" in navigator) registerSW()
-
-useLocator({ targetIDE: "vscode" })
 
 mount(() => <StartClient />, document)
