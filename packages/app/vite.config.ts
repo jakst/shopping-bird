9e679c5a51231a7ea85f160fc8bdfd9c36b28826
Jakob Norlin <jakob.stahl91@gmail.com>
2023-02-16 20:37:03 +0100

    Switch out remaining icons

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -53,8 +53,8 @@ export default defineConfig({
 				theme_color: "#0369a1",
 				background_color: "#fff",
 				icons: [
-					{ src: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
-					{ src: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+					{ src: "/icon-x192.png", sizes: "192x192", type: "image/png" },
+					{ src: "/icon-x512.png", sizes: "512x512", type: "image/png" },
 					{ src: "/maskable-icon-x192.png", sizes: "192x192", type: "image/png", purpose: "maskable" },
 					{ src: "/maskable-icon-x512.png", sizes: "512x512", type: "image/png", purpose: "maskable" },
 				],
