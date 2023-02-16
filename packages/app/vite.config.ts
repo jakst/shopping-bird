14746990cde980752c164b38b305534c5856af27
Jakob Norlin <jakob.stahl91@gmail.com>
2023-02-16 16:32:45 +0100

    Add maskable icons

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -55,6 +55,8 @@ export default defineConfig({
 				icons: [
 					{ src: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
 					{ src: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+					{ src: "/maskable-icon-x192.png", sizes: "192x192", type: "image/png", purpose: "maskable" },
+					{ src: "/maskable-icon-x512.png", sizes: "512x512", type: "image/png", purpose: "maskable" },
 				],
 			},
 		}),
