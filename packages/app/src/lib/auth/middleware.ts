2515ff858ca53111a95a257c03ee86cd0cf15b56
Jakob Norlin <jakob.stahl91@gmail.com>
2023-02-16 12:12:04 +0100

    Try middleware without secretEnv

--- packages/app/src/lib/auth/middleware.ts
+++ packages/app/src/lib/auth/middleware.ts
@@ -1,9 +1,16 @@
-import { FetchEvent } from "solid-start"
+import { getSession } from "@auth/solid-start"
+import { redirect, type FetchEvent } from "solid-start"
 import { MiddlewareInput } from "solid-start/entry-server"
+import { authOptions } from "./options"
 
 export function createAuthMiddleware(protectedPaths: string[]) {
 	return ({ forward }: MiddlewareInput) =>
 		async (event: FetchEvent) => {
+			if (protectedPaths.includes(new URL(event.request.url).pathname)) {
+				const session = await getSession(event.request, authOptions)
+				if (!session) return redirect("/api/auth/signin?csrf=true")
+			}
+
 			return forward(event)
 		}
 }
--- packages/app/src/lib/auth/options.ts
+++ packages/app/src/lib/auth/options.ts
@@ -1,9 +1,9 @@
 import CredentialsProvider from "@auth/core/providers/credentials"
-import { SolidAuthConfig } from "@auth/solid-start"
+import { type SolidAuthConfig } from "@auth/solid-start"
 import { z } from "zod"
-import { secretEnv } from "../secretEnv"
 
-const { AUTH_INFO, AUTH_SECRET } = secretEnv
+const AUTH_INFO = "herpaderp:flerpflerp"
+const AUTH_SECRET = "derpdeprderp"
 
 const trimmedString = z
 	.string()
