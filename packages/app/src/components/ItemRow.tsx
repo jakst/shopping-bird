3433f9945f66585be68beb8f3f610b32e61a32d9
Jakob Norlin <jakob.stahl91@gmail.com>
2023-03-29 22:37:23 +0200

    Add drag handlers for sorting items

--- packages/app/src/components/ItemRow.tsx
+++ packages/app/src/components/ItemRow.tsx
@@ -1,6 +1,7 @@
-import { createSortable, useDragDropContext } from "@thisbeyond/solid-dnd"
+import { createSortable, transformStyle, useDragDropContext } from "@thisbeyond/solid-dnd"
 import { type ShoppingListItem } from "lib"
-import { Show, createSignal } from "solid-js"
+import { Show, batch, createSignal } from "solid-js"
+import IconPadding from "~icons/ci/drag-vertical"
 import IconTrash from "~icons/ci/trash-full"
 import { Button } from "./Button"
 
@@ -10,9 +11,11 @@ interface Actions {
 	renameItem: (id: string, name: string) => void
 }
 
+const [focusingSomeRow, setFocusingSomeRow] = createSignal(false)
+
 export function ItemRow(props: { item: ShoppingListItem; actions: Actions }) {
-	const [hovering, setHovering] = createSignal(false)
-	const [focusing, setFocusing] = createSignal(false)
+	const [hoveringThisRow, setHoveringThisRow] = createSignal(false)
+	const [focusingThisRow, setFocusingThisRow] = createSignal(false)
 
 	const [newName, setNewName] = createSignal(props.item.name)
 
@@ -27,66 +30,100 @@ export function ItemRow(props: { item: ShoppingListItem; actions: Actions }) {
 	const dragDropContext = useDragDropContext()
 	const sortable = dragDropContext ? createSortable(props.item.id, props.item) : undefined
 
-	const showingActions = () => (hovering() || focusing()) && !sortable?.isActiveDraggable
+	const showingActions = () => {
+		if (props.item.checked) return false
+		if (focusingSomeRow() && !focusingThisRow()) return false
+
+		return focusingThisRow() || hoveringThisRow()
+	}
+
+	const onFocusIn = () => {
+		batch(() => {
+			setFocusingSomeRow(true)
+			setFocusingThisRow(true)
+		})
+	}
+
+	const onFocusOut = () => {
+		setFocusingSomeRow(false)
+		// Drop focus on a delay to fix issue with press on delete button
+		// not being registered on mobile Safari.
+		setTimeout(() => setFocusingThisRow(false), 100)
+	}
 
 	return (
 		<li
-			ref={(ref) => sortable?.(ref)}
-			class="flex px-1 items-center justify-between overflow-hidden shrink-0 cursor-move touch-none"
-			classList={{ "transition-transform": dragDropContext ? !!dragDropContext[0].active.draggable : false }}
-			style={sortable?.isActiveDraggable ? { opacity: 0.25 } : undefined}
-			onPointerEnter={() => setHovering(true)}
-			onPointerLeave={() => setHovering(false)}
-			onFocusIn={() => setFocusing(true)}
-			onFocusOut={(event) => {
-				submitNameChange()
-
-				if (!(event.relatedTarget && event.currentTarget.contains(event.relatedTarget as any))) {
-					// Drop focus on a delay to fix issue with press on delete button
-					// not being registered on mobile Safari.
-					setTimeout(() => setFocusing(false), 100)
-				}
+			ref={(ref) => sortable?.ref(ref)}
+			class="flex"
+			style={sortable && transformStyle(sortable.transform)}
+			classList={{
+				"transition-transform": dragDropContext ? !!dragDropContext[0].active.draggable : false,
+				"opacity-25": sortable?.isActiveDraggable,
 			}}
 		>
-			<div class="flex flex-1">
-				<label class="p-3 h-10 aspect-square flex items-center justify-center">
-					<input
-						class="w-5 h-5"
-						type="checkbox"
-						checked={props.item.checked}
-						onChange={(event) => {
-							props.actions.setChecked(props.item.id, event.currentTarget.checked)
-						}}
-					/>
-				</label>
+			<div
+				class="flex flex-1 items-center justify-between overflow-hidden shrink-0"
+				onPointerEnter={() => setHoveringThisRow(true)}
+				onPointerLeave={() => setHoveringThisRow(false)}
+				onFocusIn={onFocusIn}
+				onFocusOut={(event) => {
+					submitNameChange()
 
-				<form
-					class="flex flex-1"
-					onSubmit={(event) => {
-						event.preventDefault()
-						// Blur will trigger lost focus, and therefore submit the name change
-						;(event.currentTarget[0] as HTMLInputElement)?.blur()
-					}}
-				>
-					<input
-						ref={nameInputField}
-						value={props.item.name}
-						style={{ "-webkit-tap-highlight-color": "transparent" }}
-						spellcheck={false}
-						class="flex-1 bg-transparent overflow-ellipsis focus:outline-none focus:underline"
-						classList={{
-							"line-through": props.item.checked,
+					if (!(event.relatedTarget && event.currentTarget.contains(event.relatedTarget as any))) {
+						onFocusOut()
+					}
+				}}
+			>
+				<div class="flex flex-1">
+					<label class="p-3 h-10 aspect-square flex items-center justify-center">
+						<input
+							class="w-5 h-5"
+							type="checkbox"
+							checked={props.item.checked}
+							onChange={(event) => {
+								props.actions.setChecked(props.item.id, event.currentTarget.checked)
+							}}
+						/>
+					</label>
+
+					<form
+						class="flex flex-1"
+						onSubmit={(event) => {
+							event.preventDefault()
+							// Blur will trigger lost focus, and therefore submit the name change
+							;(event.currentTarget[0] as HTMLInputElement)?.blur()
 						}}
-						onInput={(event) => setNewName(event.currentTarget.value)}
-					/>
-				</form>
+					>
+						<input
+							ref={nameInputField}
+							value={props.item.name}
+							style={{ "-webkit-tap-highlight-color": "transparent" }}
+							spellcheck={false}
+							class="flex-1 bg-transparent overflow-ellipsis focus:outline-none focus:underline"
+							classList={{
+								"line-through": props.item.checked,
+							}}
+							onInput={(event) => setNewName(event.currentTarget.value)}
+						/>
+					</form>
+				</div>
+
+				<Show when={showingActions()}>
+					<div class="flex items-center">
+						<Button onClick={() => props.actions.deleteItem(props.item.id)}>
+							<IconTrash height="100%" />
+						</Button>
+					</div>
+				</Show>
 			</div>
 
-			<Show when={showingActions()}>
-				<div class="ml-1 mr-2 flex items-center">
-					<Button onClick={() => props.actions.deleteItem(props.item.id)}>
-						<IconTrash height="100%" />
-					</Button>
+			<Show when={!props.item.checked}>
+				<div
+					class="flex items-center px-4 cursor-move touch-none transition-opacity duration-300"
+					{...sortable?.dragActivators}
+					classList={{ "opacity-20": focusingSomeRow() }}
+				>
+					<IconPadding class="opacity-40" />
 				</div>
 			</Show>
 		</li>
