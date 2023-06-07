8e4dba5a81d29174b83c80318805be0e5a00814a
Jakob Norlin <jakob.stahl91@gmail.com>
2023-06-07 19:46:25 +0200

    Fix broken sorting

--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -89,7 +89,7 @@ function Home() {
 	const { client, items } = createClient()
 
 	const sortedList = () => {
-		return items.sort((a, b) => a.position - b.position)
+		return [...items].sort((a, b) => a.position - b.position)
 	}
 
 	const activeList = () => sortedList().filter((item) => !item.checked)
