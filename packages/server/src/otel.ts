2abf02745381de9a15a674227025a73ccf32e1c5
Jakob Norlin <jakob.stahl91@gmail.com>
2023-07-10 16:14:36 +0200

    TEMP!: Debug otel

--- packages/server/src/otel.ts
+++ packages/server/src/otel.ts
@@ -5,14 +5,16 @@ import * as Tracer from "@effect/opentelemetry/Tracer"
 import { OTLPTraceExporter as OTLPTraceExporterHttp } from "@opentelemetry/exporter-trace-otlp-http"
 import { env } from "./env"
 
+export const traceExporter =
+	env.USE_LOCAL_EXPORTER === "1"
+		? new OTLPTraceExporterHttp({ url: "http://127.0.0.1:4318/v1/traces" })
+		: new OTLPTraceExporterHttp({
+				url: "https://otel.baselime.io/v1",
+				headers: { "x-api-key": env.BASELIME_API_KEY },
+		  })
+
 const NodeSdkLive = NodeSdk.layer({
-	traceExporter:
-		env.USE_LOCAL_EXPORTER === "1"
-			? new OTLPTraceExporterHttp({ url: "http://127.0.0.1:4318/v1/traces" })
-			: new OTLPTraceExporterHttp({
-					url: "https://otel.baselime.io/v1",
-					headers: { "x-api-key": env.BASELIME_API_KEY },
-			  }),
+	traceExporter,
 })
 
 const ResourceLive = Resource.layer({ serviceName: "backend" })
