d6c8f31dc18292d211c3b60aa3ff0ef1a08cebd1
Jakob Norlin <jakob.stahl91@gmail.com>
2024-12-22 11:11:42 +0100

    Only preload in prod
    And correct URL

--- packages/app/src/entry-server.tsx
+++ packages/app/src/entry-server.tsx
@@ -13,9 +13,9 @@ export default createHandler(() => (
 					<meta name="description" content="Your shopping list" />
 
 					<link rel="icon" type="image/png" href="/favicon.png" />
-					<link rel="preload" href="/_build/Hubot-Sans-1.0.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
 
 					<Show when={import.meta.env.PROD}>
+						<link rel="preload" href="/Hubot-Sans-1.0.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
 						<link rel="manifest" type="application/manifest+json" href="/manifest.webmanifest" />
 					</Show>
 
